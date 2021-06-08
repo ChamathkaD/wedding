dc3c80b6a01ce7179c757df28b9bbac4c10fdac9
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-06-08 20:09:59 +0530

    Admin Table Changed

--- database/migrations/2021_05_26_104138_create_admins_table.php
+++ database/migrations/2021_05_26_104138_create_admins_table.php
@@ -18,9 +18,19 @@ class CreateAdminsTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('image')->default("default.png");
+            $table->string('phone')->unique()->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('country')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamp('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
