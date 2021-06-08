84eadb509e1e4c92b07c7ed7a5d6aa40ab174f02
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-06-08 21:32:51 +0530

    login form updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!--<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,7 +15,15 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input
+                                    id="email"
+                                    type="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    required
+                                    autocomplete="email"
+                                    autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +37,13 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    name="password"
+                                    required
+                                    autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -69,5 +83,106 @@
             </div>
         </div>
     </div>
+</div>-->
+
+
+
+
+
+<div class="contact-form section-padding2 fix">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 offset-lg-3 offset-xl-3">
+                <div class="form-wrapper">
+                    <!-- section tittle -->
+                    <div class="row ">
+                        <div class="col-lg-12">
+                            <div class="section-tittle tittle-form text-center">
+<!--                                <img src="{{ asset('img/memories/section_tittle_flowre.png') }}" alt="">-->
+                                <h2>{{ __('Login') }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <form id="contact-form" action="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="row">
+
+                            <div class="col-lg-12">
+                                <div class="form-box subject-icon mb-30">
+                                    <label for="email" >{{ __('E-Mail Address') }}</label>
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        name="email"
+                                        value="{{ old('email') }}"
+                                        required
+                                        autocomplete="email"
+                                    placeholder="Email">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <div class="form-box subject-icon mb-30">
+                                    <label for="email" >{{ __('Password') }}</label>
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        class="form-control @error('password') is-invalid @enderror"
+                                        name="password"
+                                        required
+                                        autocomplete="current-password"
+                                        placeholder="Password"
+                                    >
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-12">
+
+                                <div class="submit-info col-md-1 ">
+                                    <button class="btn2" type="submit"> {{ __('Login') }}</button>
+                                </div>
+
+                                <div class="col-md-9 mt-3">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn2" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </form>
+                    <!-- Shape inner Flower -->
+                    <div class="shape-inner-flower">
+                        <img src="{{ asset('img/flower/form-smoll-left.png') }}" class="top1" alt="">
+                        <img src="{{ asset('img/flower/form-smoll-right.png') }}" class="top2" alt="">
+                        <img src="{{ asset('img/flower/form-smoll-b-left.png') }}"class="top3"  alt="">
+                        <img src="{{ asset('img/flower/form-smoll-b-right.png') }}"class="top4"  alt="">
+                    </div>
+                    <!-- Shape outer Flower -->
+                    <div class="shape-outer-flower">
+                        <img src="{{ asset('img/flower/from-top.png') }}" class="outer-top" alt="">
+                        <img src="{{ asset('img/flower/from-bottom.png') }}" class="outer-bottom" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!--<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -39,6 +39,71 @@
                             </div>
                         </div>
                     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>-->
+
+
+<div class="contact-form section-padding2 fix">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 offset-lg-3 offset-xl-3">
+                <div class="form-wrapper">
+                    <!-- section tittle -->
+                    <div class="row ">
+                        <div class="col-lg-12">
+                            <div class="section-tittle tittle-form text-center">
+<!--                                <img src="assets/img/memories/section_tittle_flowre.png" alt="">-->
+                                <h2>{{ __('Reset Password') }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <form id="contact-form" action="{{ route('password.email') }}" method="POST">
+                        @csrf
+                        <div class="row">
+
+                            <div class="col-lg-12">
+                                <div class="form-box subject-icon mb-30">
+                                    <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        name="email"
+                                        value="{{ old('email') }}"
+                                        required autocomplete="email"
+                                        autofocus
+                                        placeholder="Email">
+
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+
+                                <div class="submit-info">
+                                    <button class="btn2" type="submit"> {{ __('Send Password Reset Link') }}</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- Shape inner Flower -->
+                    <div class="shape-inner-flower">
+                        <img src="{{ asset('img/flower/form-smoll-left.png') }}" class="top1" alt="">
+                        <img src="{{ asset('img/flower/form-smoll-right.png') }}" class="top2" alt="">
+                        <img src="{{ asset('img/flower/form-smoll-b-left.png') }}"class="top3"  alt="">
+                        <img src="{{ asset('img/flower/form-smoll-b-right.png') }}"class="top4"  alt="">
+                    </div>
+                    <!-- Shape outer Flower -->
+                    <div class="shape-outer-flower">
+                        <img src="{{ asset('img/flower/from-top.png') }}" class="outer-top" alt="">
+                        <img src="{{ asset('img/flower/from-bottom.png') }}" class="outer-bottom" alt="">
+                    </div>
                 </div>
             </div>
         </div>
