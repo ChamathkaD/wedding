be063b7902a83a33c224a5ffbcd638ff59409276
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-06-09 10:43:24 +0530

    Cleared Dashboard

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,22 +2,19 @@
     <nav class="navbar top-navbar navbar-expand-md navbar-dark">
         <div class="navbar-header" data-logobg="skin5">
 
-            <!-- ============================================================== -->
-            <!-- Logo -->
-            <!-- ============================================================== -->
             <a class="navbar-brand" href="index.html">
                 <!-- Logo icon -->
                 <b class="logo-icon ps-2">
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
-                    <img src="../../assets/images/logo-icon.png" alt="homepage" class="light-logo" />
+                    <img src="{{ asset('dashboard/assets/images/logo-icon.png') }}" alt="homepage" class="light-logo" />
 
                 </b>
                 <!--End Logo icon -->
                 <!-- Logo text -->
                 <span class="logo-text">
                             <!-- dark Logo text -->
-                            <img src="../../assets/images/logo-text.png" alt="homepage" class="light-logo" />
+                            <img src="{{ asset('dashboard/assets/images/logo-text.png') }}" alt="homepage" class="light-logo" />
 
                         </span>
                 <!-- Logo icon -->
@@ -29,18 +26,9 @@
                 <!-- </b> -->
                 <!--End Logo icon -->
             </a>
-            <!-- ============================================================== -->
-            <!-- End Logo -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- Toggle which is visible on mobile only -->
-            <!-- ============================================================== -->
             <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
                     class="ti-menu ti-close"></i></a>
         </div>
-        <!-- ============================================================== -->
-        <!-- End Logo -->
-        <!-- ============================================================== -->
         <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
             <!-- ============================================================== -->
             <!-- toggle and nav items -->
@@ -169,7 +157,7 @@
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="{{ asset('dashboard/assets/images/users/1.jpg') }}" alt="user" class="rounded-circle" width="31">
+                        <img src="{{ asset('dashboard/assets/images/users/1.jpg') }}" alt="user" class="rounded-circle me-2" width="31">
                         {{ Auth::guard('admin')->user()->name }}
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
