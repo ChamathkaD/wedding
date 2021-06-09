99b98d9dcca11dbef9c7bfdf4d4103edbd40fb24
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-06-09 11:27:44 +0530

    Cleared Dashboard V2

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -40,28 +40,11 @@
                 <!-- ============================================================== -->
                 <!-- create new -->
                 <!-- ============================================================== -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="d-none d-md-block">Create New <i class="fa fa-angle-down"></i></span>
-                        <span class="d-block d-md-none"><i class="fa fa-plus"></i></span>
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+                @include('admin.partials.create-new')
                 <!-- ============================================================== -->
                 <!-- Search -->
                 <!-- ============================================================== -->
-                <li class="nav-item search-box"> <a class="nav-link waves-effect waves-dark"
-                                                    href="javascript:void(0)"><i class="ti-search"></i></a>
-                    <form class="app-search position-absolute">
-                        <input type="text" class="form-control" placeholder="Search &amp; enter"> <a
-                            class="srh-btn"><i class="ti-close"></i></a>
-                    </form>
-                </li>
+                @include('admin.partials.search')
             </ul>
             <!-- ============================================================== -->
             <!-- Right side toggle and nav items -->
@@ -160,27 +143,7 @@
                         <img src="{{ asset('dashboard/assets/images/users/1.jpg') }}" alt="user" class="rounded-circle me-2" width="31">
                         {{ Auth::guard('admin')->user()->name }}
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user me-1 ms-1"></i>
-                            My Profile</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-wallet me-1 ms-1"></i>
-                            My Balance</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i class="ti-email me-1 ms-1"></i>
-                            Inbox</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                class="ti-settings me-1 ms-1"></i> Account Setting</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="{{ route('admin.logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('admin-logout-form').submit();" ><i
-                                class="fa fa-power-off me-1 ms-1"></i> Logout</a>
-                        <form id="admin-logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                        <div class="dropdown-divider"></div>
-                        <div class="ps-4 p-10"><a href="javascript:void(0)"
-                                                  class="btn btn-sm btn-success btn-rounded text-white">View Profile</a></div>
-                    </ul>
+                    @include('admin.partials.main-dropdown')
                 </li>
 
                 @endauth
