67c8f2443463f0421c94981114e84c46360aa2d4
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-06-16 20:08:07 +0530

    Sidebar updated

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,27 +4,54 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav" class="pt-4">
-                <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                            href="index.html" aria-expanded="false"><i
-                            class="mdi mdi-view-dashboard"></i><span
-                            class="hide-menu">Dashboard</span></a></li>
-                <li class="sidebar-item"><a class="sidebar-link has-arrow waves-effect waves-dark"
-                                            href="javascript:void(0)" aria-expanded="false"><i
-                            class="mdi mdi-receipt"></i><span
-                            class="hide-menu">Forms </span></a>
-                    <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="form-basic.html" class="sidebar-link"><i
-                                    class="mdi mdi-note-outline"></i><span class="hide-menu"> Form Basic
-                                        </span></a></li>
-                        <li class="sidebar-item"><a href="form-wizard.html" class="sidebar-link"><i
-                                    class="mdi mdi-note-plus"></i><span class="hide-menu"> Form Wizard
-                                        </span></a></li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                       href="{{ route('admin.dashboard') }}"
+                       aria-expanded="false">
+                        <i class="mdi mdi-view-dashboard"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                       href="{{ route('admin.users.index') }}"
+                       aria-expanded="false">
+                        <i class="mdi mdi-account-multiple"></i>
+                        <span class="hide-menu">Users</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow waves-effect waves-dark"
+                       href="javascript:void(0)"
+                       aria-expanded="false">
+                        <i class="mdi mdi-receipt"></i>
+                        <span class="hide-menu">Forms</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="form-basic.html" class="sidebar-link">
+                                <i class="mdi mdi-note-outline"></i>
+                                <span class="hide-menu">Form Basic</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="form-wizard.html" class="sidebar-link">
+                                <i class="mdi mdi-note-plus"></i>
+                                <span class="hide-menu">Form Wizard</span>
+                            </a>
+                        </li>
                     </ul>
                 </li>
                 <li class="sidebar-item p-3">
-                    <a href="https://github.com/wrappixel/matrix-admin-bt5" target="_blank"
-                       class="w-100 btn btn-cyan d-flex align-items-center text-white"><i
-                            class="mdi mdi-cloud-download font-20 me-2"></i>Download Free</a>
+                    <a href="https://github.com/wrappixel/matrix-admin-bt5"
+                       target="_blank"
+                       class="w-100 btn btn-cyan d-flex align-items-center text-white">
+                        <i class="mdi mdi-cloud-download font-20 me-2"></i>
+                        Download Free
+                    </a>
                 </li>
             </ul>
         </nav>
