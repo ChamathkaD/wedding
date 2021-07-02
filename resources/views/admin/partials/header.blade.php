1dc28aaeb4c6ec77fdc7e1fb61dceaa26e2f6d59
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-02 09:36:10 +0530

    header.blade.php updated

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -30,25 +30,17 @@
                     class="ti-menu ti-close"></i></a>
         </div>
         <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
-            <!-- ============================================================== -->
-            <!-- toggle and nav items -->
-            <!-- ============================================================== -->
+
             <ul class="navbar-nav float-start me-auto">
                 <li class="nav-item d-none d-lg-block"><a
                         class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)"
                         data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a></li>
-                <!-- ============================================================== -->
-                <!-- create new -->
-                <!-- ============================================================== -->
+
                 @include('admin.partials.create-new')
-                <!-- ============================================================== -->
-                <!-- Search -->
-                <!-- ============================================================== -->
+
                 @include('admin.partials.search')
             </ul>
-            <!-- ============================================================== -->
-            <!-- Right side toggle and nav items -->
-            <!-- ============================================================== -->
+
             <ul class="navbar-nav float-end">
                 <!-- ============================================================== -->
                 <!-- Comment -->
