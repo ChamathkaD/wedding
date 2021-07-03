f105e3a63a2d123c71f0a93cb8a49bb03a5af095
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-03 16:00:00 +0530

    sidebar.blade.php modified

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -18,77 +18,37 @@
                     <a class="sidebar-link has-arrow waves-effect waves-dark"
                        href="javascript:void(0)"
                        aria-expanded="false">
-                        <i class="mdi mdi-receipt"></i>
+                        <i class="mdi mdi-account-multiple"></i>
                         <span class="hide-menu">Users</span>
                     </a>
                     <ul aria-expanded="false" class="collapse first-level">
                         <li class="sidebar-item">
                             <a href="{{ route('admin.users.requests') }}" class="sidebar-link">
-                                <i class="mdi mdi-note-outline"></i>
+                                <i class="mdi mdi-account-plus"></i>
                                 <span class="hide-menu">New Requests</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
                             <a href="{{ route('admin.users.index') }}" class="sidebar-link">
-                                <i class="mdi mdi-note-plus"></i>
+                                <i class="mdi mdi-account-multiple"></i>
                                 <span class="hide-menu">All Users</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
                             <a href="{{ route('admin.users.blocked') }}" class="sidebar-link">
-                                <i class="mdi mdi-note-plus"></i>
+                                <i class="mdi mdi-account-off"></i>
                                 <span class="hide-menu">Blocked Users</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
                             <a href="#" class="sidebar-link">
-                                <i class="mdi mdi-note-plus"></i>
+                                <i class="mdi mdi-account-remove"></i>
                                 <span class="hide-menu">Deleted Users</span>
                             </a>
                         </li>
                     </ul>
                 </li>
 
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                       href="#"
-                       aria-expanded="false">
-                        <i class="mdi mdi-account-multiple"></i>
-                        <span class="hide-menu">Users</span>
-                    </a>
-                </li>
-
-                <li class="sidebar-item">
-                    <a class="sidebar-link has-arrow waves-effect waves-dark"
-                       href="javascript:void(0)"
-                       aria-expanded="false">
-                        <i class="mdi mdi-receipt"></i>
-                        <span class="hide-menu">Forms</span>
-                    </a>
-                    <ul aria-expanded="false" class="collapse first-level">
-                        <li class="sidebar-item">
-                            <a href="form-basic.html" class="sidebar-link">
-                                <i class="mdi mdi-note-outline"></i>
-                                <span class="hide-menu">Form Basic</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="form-wizard.html" class="sidebar-link">
-                                <i class="mdi mdi-note-plus"></i>
-                                <span class="hide-menu">Form Wizard</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-
-                <li class="sidebar-item p-3">
-                    <a href="https://github.com/wrappixel/matrix-admin-bt5"
-                       target="_blank"
-                       class="w-100 btn btn-cyan d-flex align-items-center text-white">
-                        <i class="mdi mdi-cloud-download font-20 me-2"></i>
-                        Download Free
-                    </a>
-                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
