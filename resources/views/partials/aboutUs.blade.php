13688464ea70bcfd79709a72319fc1e02926d02d
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-07-03 10:31:16 +0530

    about and contact us created & designed

---
+++ resources/views/partials/aboutUs.blade.php
@@ -0,0 +1,212 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="slider-area ">
+    <!-- Mobile Menu -->
+    <div class="single-slider slider-height2  hero-overly d-flex align-items-center" data-background="{{ asset('img/hero/about_hero.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="hero-cap text-center">
+                        <h2>About Us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="Our-story-area story-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="story-img mb-100">
+                    <img src="{{ asset('img/our_story/story-img.jpg') }}" class="story-imges" alt="">
+                    <!-- shape flower -->
+<!--                    <div class="shape-flower-img">
+                        <img src="assets/img/our_story/flower_top.png" class="flower-top" alt="">
+                    </div>-->
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="story-caption">
+<!--                    <img src="{{ asset('img/our_story/flower_right.png') }}" alt="">-->
+                    <h3>Make Your Marriage Dream Come True</h3>
+    <p class="story1">Choose the right partner for you, regardless of your status, from our wedding proposals</p>
+                    <p class="story1">Through Sri Lanka's premier and most reliable wedding service, we have made it possible for you to find a suitable partner for your daughter, son, brother, sister, friend, age, caste, religion, race, education, income level and many more.</p>
+                    <p class="story2">Do not Waste Your Valuable Time & Money on Matchmakers or News Paper Advertisements. Come, Join Us Now...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- shape -->
+    <div class="shape-flower d-none d-xl-block">
+        <img src="{{ asset('img/our_story/shape_left.png') }}" class="flower1" alt="">
+        <img src="{{ asset('img/our_story/shape_right.png') }}" class="flower2 " alt="">
+    </div>
+</div>
+
+
+
+<div class="container">
+    <div class="story-caption">
+
+        <h1 class="text-center mb-5">HOW TO REGISTER</h1>
+    </div>
+    <div class="row">
+
+
+
+
+        <div class="col-lg-4">
+            <div class="card text-center mb-60 card-hover shadow-sm background ">
+                <div class="card-body " style="background: #fbf9ff">
+
+                    <div class="media-body">
+                        <h3>01.CREATE AN ACCOUNT</h3>
+                        <p>Register for a new account filling the application provided in our website.(Registration fee: 500 LKR)</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="card text-center mb-60 card-hover shadow-sm background ">
+                <div class="card-body " style="background: #fbf9ff">
+
+                    <div class="media-body">
+                        <h3>02. LOG IN TO YOUR ACCOUNT</h3>
+                        <p>Verify your account using your email address.</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card text-center mb-60 card-hover shadow-sm background ">
+                <div class="card-body " style="background: #fbf9ff">
+
+                    <div class="media-body">
+                        <h3>03. GET YOUR ACCOUNT VERIFIED</h3>
+                        <p>We will confirm your account within 48 hours after consideration of your information.</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="service-area ">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="singl-services text-center mb-60">
+                    <div class="top-caption">
+                        <h4>About Registration Fee</h4>
+
+                    </div>
+
+                    <div class="bottom-caption">
+<!--                        <span>12:00PM-2:00PM</span>-->
+                        <p>When you register this site yourself or through a broker, you must payed 500LKR for registration.You can deposit your money through a Bank deposit, online banking, deposit machine or eZ Cash
+                            A snapshot of your bank slip or a screenshot of your online payment must be uploaded.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+
+<div class="service-area ">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="singl-services text-center mb-60">
+                    <div class="top-caption">
+                        <h4>About Broker</h4>
+
+                    </div>
+
+                    <div class="bottom-caption">
+                        <!--                        <span>12:00PM-2:00PM</span>-->
+                        <p>It is completely free to register as a broker.Your role as a broker is to register users on our website.If a person registers through a broker, the broker can profit from our website.You can earn more money from Chamara if you can register more users for our site.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+<div class="gift-area gift-padding gift-overly" data-background="{{ asset('img/memories/gft.jpg') }}">
+    <div class="container">
+        <div class="row">
+            <div class=" offset-lg-1 col-12">
+                <div class="gift-caption">
+                    <p> CUSTOMER SERVICE</p>
+
+                    <h5>
+                        If you have any problems while using the Chamar website or before opening an account, <br> you can contact our Customer Service on 07125678.</h5>
+
+                    <h5>You can report any problems by calling or texting this number. In addition, you can send <br>your problems via whatsapp .</h5>
+
+
+                    <h5>We look forward to your help in identifying the fake accounts in this group. <br>After exchanging phone numbers,
+                        you can also alert customer service about accounts that may <br> have inappropriately / subtitled pornographic images or entered false information.
+                    </h5>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="Our-story-area story-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="story-img mb-100">
+                    <img src="{{ asset('img/our_story/story-img.jpg') }}" class="story-imges" alt="">
+                    <!-- shape flower -->
+                    <!--                    <div class="shape-flower-img">
+                                            <img src="assets/img/our_story/flower_top.png" class="flower-top" alt="">
+                                        </div>-->
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="story-caption">
+                <!--                    <img src="{{ asset('img/our_story/flower_right.png') }}" alt="">-->
+                    <h3>Continuous trust
+                    </h3>
+                    <p class="story1">Choose the right partner for you, regardless of your status, from our wedding proposals</p>
+                    <p class="story1">Your phone number will be used to instantly notify you via sms of suggestions and invitations that suit you. Under no circumstances will your phone number be transferred to another party without your knowledge.</p>
+                    <p class="story2">your suggestions and ideas are very important to make this service even better. In addition, we look forward to your assistance in identifying fraudulent accounts within this group. After exchanging phone numbers, we will not hesitate to remove from your account any accounts that may have caused you illicit / pornographic photos or false information. To do so, click on the 'Complain This Account' button on the phone number sharing page and send us your query regarding the relevant account. We hope this will enable us to provide a better quality service.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- shape -->
+    <div class="shape-flower d-none d-xl-block">
+        <img src="{{ asset('img/our_story/shape_left.png') }}" class="flower1" alt="">
+        <img src="{{ asset('img/our_story/shape_right.png') }}" class="flower2 " alt="">
+    </div>
+</div>
+@endsection
