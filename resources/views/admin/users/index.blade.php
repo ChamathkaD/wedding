c920c248535db32ebf34ea2077532468c56948a9
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-07-16 22:08:38 +0530

    index.blade.php updated

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -27,23 +27,27 @@
                             <table id="zero_config" class="table table-striped table-bordered">
                                 <thead>
                                 <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Salary</th>
+                                    <th>first_name</th>
+                                    <th>last_name</th>
+                                    <th>email</th>
+                                    <th>phone</th>
+                                    <th>birthday</th>
+                                    <th>Action</th>
+
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @foreach($records as $record)
                                 <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>System Architect</td>
-                                    <td>Edinburgh</td>
-                                    <td>61</td>
-                                    <td>2011/04/25</td>
-                                    <td>$320,800</td>
+                                    <td>{{$record->first_name}}</td>
+                                    <td>{{$record->last_name}}</td>
+                                    <td>{{$record->email}}</td>
+                                    <td>{{$record->phone}}</td>
+                                    <td>{{$record->birthday}}</td>
+                                    <td>view</td>
+
                                 </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
